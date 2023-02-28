9aa7fd2ebbcbe6e5df6b76a3d149caa6fcf8cc56
Miwako <you@example.com>
2023-02-28 13:55:12 +0000

    finissh kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,46 +1,51 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
+  def top
+ redirect_to book_path(book.id)
   end
-  
+
  def create
    book = Book.new(book_params)
     book.save
- redirect_to '/top'
+
+    flash[:notice] = "投稿が成功しました"
+ redirect_to book_path(book.id)
+
+
  end
-  
+
   def index
     @books = Book.all
+    @book = Book.new
 
-    
-    flash[:notice] = "投稿が成功しました"
-    redirect_to book_path(book.id)
+
   end
 
 
     def show
-        @books = Book.find(params[:id])
+        @book = Book.find(params[:id])
     end
 
   def edit
      @book = Book.find(params[:id])
   end
-   
+
      def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
      end
-    
+
     def destroy
       book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
     redirect_to '/books'
     end
-  
+
+
      private
-     
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
