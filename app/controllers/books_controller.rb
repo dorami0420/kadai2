56ac0e234bddfdf0621e0837dc797a68dfac98e6
Miwako <you@example.com>
2023-03-02 13:31:40 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,58 +1,67 @@
 class BooksController < ApplicationController
   def top
- redirect_to book_path(book.id)
+
   end
 
-def new
+  def new
     @book = Book.new
 
-end
+  end
 
- def create
+  def create
    @book = Book.new(book_params)
    if @book.save
-      flash[:notice] = "投稿が成功しました"
-redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
    else
       @books = Book.all
-  render :index
+      render :index
    end
-
-
-
- end
+  end
 
   def index
     @books = Book.all
     @book = Book.new
-
-
   end
 
 
-    def show
-        @book = Book.find(params[:id])
-
-    end
+  def show
+    @book = Book.find(params[:id])
+  end
 
   def edit
      @book = Book.find(params[:id])
-  end
 
-     def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
-     end
 
-    def destroy
-      book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to '/books'
+
+
+      @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
     end
 
 
-     private
+
+
+  end
+
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+
+  private
 
   def book_params
     params.require(:book).permit(:title, :body)
