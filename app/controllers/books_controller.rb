576b4a1aed5886de0324436bbb6553ffb9d2ccf6
Miwako <you@example.com>
2023-03-04 07:06:37 +0000

    i

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,27 +32,19 @@ class BooksController < ApplicationController
   def edit
      @book = Book.find(params[:id])
 
-
-
-
-      @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(@book.id)
-    else
-      render :new
-    end
-
-
-
-
   end
 
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+        redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
+
+
 
   def destroy
     book = Book.find(params[:id])
@@ -66,5 +58,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
-end
+  end
